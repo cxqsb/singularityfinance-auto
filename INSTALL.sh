95e68db43af194418c4f7fc6f9e04d7ab3316b17
cxqsb <508951305@qq.com>
2025-02-03 22:50:08 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
+
+# 激活虚拟环境
+if [ -d "./.venv" ]; then
+    source ./.venv/bin/activate
+else
+    echo "creating env..."
+    python3 -m venv .venv
+    source ./.venv/bin/activate
+    pip install -r requirements.txt
+fi
+
+# 运行Python脚本来输入私钥
+python3 << EOF
 def append_private_key(file_path):
     while True:
         # 提示用户输入私钥
         private_key = input("请输入私钥（或输入 'exit' 退出）: ")
-        
+
         # 检查是否输入 'exit' 以退出循环
         if private_key.lower() == 'exit':
             break
-        
+
         # 将私钥写入文件，并在每个私钥后添加换行符
         with open(file_path, 'a') as file:
             file.write(private_key + '\n')
         print("私钥已保存。")
-        
+
         # 提示用户输入下一个私钥
         print("请输入下一个私钥。")
 
@@ -22,13 +35,4 @@ if __name__ == "__main__":
     
     # 追加私钥到文件
     append_private_key(file_path)
-
-if [ -d "./.venv" ]; then
-    source ./.venv/bin/activate
-else
-    echo "creating env..."
-    python3 -m venv .venv
-    source ./.venv/bin/activate
-fi
-
-pip install -r requirements.txt
+EOF
